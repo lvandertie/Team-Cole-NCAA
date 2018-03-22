a42ee7ee9be466b9c5025e44449f54050b560df1
Cole Heflin <cole.heflin@yahoo.com>
2018-03-21 18:45:27 -0700

    Initial Agreggation
    PPG, APG

--- Summarize.py
+++ Summarize.py
@@ -4,5 +4,18 @@ import numpy as np
 df = pd.read_csv('DataFiles/RegularSeasonDetailedResults.csv')
 
 
+# Select columns for regression
+winners = df[['Season','WTeamID', 'WScore', 'WAst']]
+# Rename columns
+winners = winners.rename(columns={'WTeamID': 'TeamID', 'WScore': 'Score', 'WAst' : 'Assist'})
 
-print(df.groupby(['Season', 'WTeamID']).mean())
+losers = df[['Season', 'LTeamID', 'LScore', 'LAst']]
+losers = losers.rename(columns={'LTeamID': 'TeamID', 'LScore': 'Score', 'LAst' : 'Assist'})
+
+total = pd.concat([winners, losers])
+
+total = total.groupby(['Season', 'TeamID']).mean()
+
+print(total)
+
+
